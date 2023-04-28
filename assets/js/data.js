1d890c18b20014bb0eeeec761384d0b9d1138899
unknown <nataliematos12@gmail.com>
2023-04-28 01:55:26 -0400

    Added questions to quiz and updated names of some files

---
+++ assets/js/data.js
@@ -0,0 +1,90 @@
+var questions = [
+    {
+        question: 'What are the primitive data types in JavaScript?',
+        answerIndex: 1,
+        options: [
+            'Integer, Null, LilInt, Boolean, String, Symbol, and Undefined',
+            'String, Number, BigInt, Boolean, Symbol, Null, and Undefined',
+            'Number, Undefined, Boolean, Symbol, String, and BigInt',
+            'String, Integer, BigInt, Bullean, Symbol, and Undefined'
+        ],
+        
+    },
+    {
+        question: 'Which is a non-primitive data type in JavaScript?',
+        answerIndex: 3,
+        options: [
+            'Boolean',
+            'Integer',
+            'Symbol',
+            'Object'
+        ],
+        
+    },
+    {
+        question: 'Which defines a object?',
+        answerIndex: 0,
+        options: [
+            'Data type used to store a collection of data',
+            'Data type used to store a collection of items',
+            'Data type used to store an ordered list',
+            'Data type used to store a collection of cookies'
+        ],
+        
+    },
+    {
+        question: 'Which defines an array?',
+        answerIndex: 2,
+        options: [
+            'Array is and array',
+            'Data displayed as an object',
+            'Data displayed as an ordered list',
+            'Arrays are booleans'
+        ],
+        
+    },
+    {
+        question: 'What does "==" mean?',
+        answerIndex: 0,
+        options: [
+            'It means that the variables are equal in value',
+            'It means that the variables are super equal to each other',
+            'It means that the variables are equal in value and type',
+            'It means that the variables are equal in type'
+        ],
+        
+    },
+    {
+        question: 'What does "===" mean?',
+        answerIndex: 1,
+        options: [
+            'It means that the variables are equal in type',
+            'It means that the variables are equal in value and type',
+            'It means that the variables are equal in value',
+            'It means that the variables are super equal to each other'
+        ],
+        
+    },
+    {
+        question: 'Which symbol shows the OR operator?',
+        answerIndex: 3,
+        options: [
+            '##',
+            '&&',
+            '%%',
+            '||'
+        ],
+        
+    },
+    {
+        question: 'Which symbol shows the AND operator?',
+        answerIndex: 1,
+        options: [
+            '##',
+            '&&',
+            '%%',
+            '||'
+        ],
+        
+    },
+];
